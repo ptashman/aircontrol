cfde5d87b753e3272090f6206d7d18937fc03aa6
Paul Tashman <paul.tashman@careerbuilder.com>
2015-04-24 18:06:25 -0400

    Adds expendability rating system to aircraft model

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -8,6 +8,31 @@ class Aircraft < ActiveRecord::Base
     self.enqueued_at = Time.now
   end
 
-#  def self.sort_by_expenability
-#    self.all
+  def passenger_rating
+    1
+  end
+
+  def cargo_rating
+    2
+  end
+
+  def large_rating
+    1
+  end
+
+  def small_rating
+    2
+  end
+
+  def rating_for(attr_name)
+    attr_value_str = send(attr_name)
+    send("#{attr_value_str.downcase}_rating".to_sym)
+  end
+
+  def self.sort_by_expendability
+    units = self.all
+    attributes = [:type, :size]
+    attributes.each { |a| units.sort! { |x,y| x.rating_for(a) <=> y.rating_for(a) } }
+    units.sort! { |x,y| x.enqueued_at <=> y.enqueued_at }
+  end
 end
