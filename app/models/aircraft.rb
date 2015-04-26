04e2570576666b95ec0876cf74a9a713c4d61c29
Paul Tashman <paul.tashman@careerbuilder.com>
2015-04-26 14:51:41 -0400

    Moves methods from aircraft to airqueue

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -13,34 +13,16 @@ class Aircraft < ActiveRecord::Base
   def enqueue(airqueue_id)
     self.airqueue_id = airqueue_id
     self.enqueued_at = Time.now
+    save
+  end
+
+  def dequeue
+    self.airqueue_id = self.enqueued_at = nil
+    save
   end
 
   def rating_for(attr_sym)
     attr_value_str = send(attr_sym)
     "Aircraft::#{attr_value_str.upcase}_RATING".constantize
-  end
-
-  def self.sort_by_expendability
-    by_category = self.all.sort { |x,y| x.rating_for(:category) <=> y.rating_for(:category) }
-    by_size = sort_in_groups(by_category, :category, :rating_for, [:size])
-    sort_in_nestings(by_size, [:category, :size], :enqueued_at)
-  end
-
-private
-
-  def self.sort_in_groups(collection, group_sym, attr_sym, params = [])
-    sorted = []
-    collection.group_by { |a| a.send(group_sym) }.each do |k,v|
-      sorted << v.sort { |x,y| x.send(attr_sym, *params) <=> y.send(attr_sym, *params) }
-    end
-    sorted.flatten
-  end
-
-  def self.sort_in_nestings(collection, group_symbols, attr_sym, params = [])
-    sorted = []
-    collection.group_by { |a| a.send(group_symbols[0]) }.each do |k,v|
-      sorted << sort_in_groups(v, group_symbols[1], attr_sym, params = [])
-    end
-    sorted.flatten
   end
 end
--- app/models/airqueue.rb
+++ app/models/airqueue.rb
@@ -1,3 +1,29 @@
 class Airqueue < ActiveRecord::Base
   has_many :aircrafts
+
+  def dequeue_one
+    sorted_aircrafts.last.dequeue
+  end
+
+  def sorted_aircrafts
+    by_category = aircrafts.sort { |x,y| x.rating_for(:category) <=> y.rating_for(:category) }
+    by_size = sorted_in_groups(by_category, :category, :rating_for, [:size])
+    sorted_in_nestings(by_size, [:category, :size], :enqueued_at)
+  end
+
+  def sorted_in_groups(collection, group_sym, attr_sym, params = [])
+    sorted = []
+    collection.group_by { |a| a.send(group_sym) }.each do |k,v|
+      sorted << v.sort { |x,y| x.send(attr_sym, *params) <=> y.send(attr_sym, *params) }
+    end
+    sorted.flatten
+  end
+
+  def sorted_in_nestings(collection, group_symbols, attr_sym, params = [])
+    sorted = []
+    collection.group_by { |a| a.send(group_symbols[0]) }.each do |k,v|
+      sorted << sorted_in_groups(v, group_symbols[1], attr_sym, params = [])
+    end
+    sorted.flatten
+  end
 end
